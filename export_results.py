15f104471c488d22ce5c9897db67554ae46bef93
adc-san <adcsan@gmail.com>
2024-12-22 20:35:33 +0500

    В экпорт результатов добавлено замену символов отсутствующих в кодировке cp1251 (если результаты содержали такие символы, то экспорт останавливался с ошибкой)

--- export_results.py
+++ export_results.py
@@ -26,7 +26,7 @@ def save_to_csv():
                 user_str = f"{first_name or ''} {last_name or ''} @{username or ''} id{str(user_id)}"
                 cp_count, cp_sum, cp_list, no_cp_list, all_cp_list = bot_utils.user_result(user_id)
                 # Записываем строку результатов в CSV, убрав запятые из данных
-                results_writer.writerow([command_name.replace(',','.'), user_str.replace(',','.')] +
+                results_writer.writerow([command_name.replace(',','.').encode('cp1251', errors='replace').decode('cp1251'), user_str.replace(',','.').encode('cp1251', errors='replace').decode('cp1251')] +
                                         ';'.join(no_cp_list.split(sep=',')).split(sep=' ') + [fin_time,] + all_cp_list.split(sep=','))
 
 print('I will save your results to {} ...'.format(config.results_filename))
